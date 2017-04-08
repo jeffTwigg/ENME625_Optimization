0e10975e15638f63667a52fa73b6add989a00eae
Jeff Twigg <jeffrey.n.twigg.civ@mail.mil>
2017-04-08 16:42:40 -0400

    working on some stuff

--- test.m
+++ test.m
@@ -14,6 +14,7 @@ h = @(X) 1 - sqrt(f1(X)/g(X));
 f2 = @(X) g(X)*h(X);
 
 
+
 F = @(X) f1(X) +f2(X) + AC;
 
 n = 30;
@@ -30,13 +31,22 @@ ub =1;
 %m=1
 %while coverageDistance(pareto_points)> 0.05
 
-pareto_points = 
+pareto_points = zeros(10,2);
+
+CD = @(X,pareto_points) coverageDifference([f1(X),f1(pareto_points(:,1))],[f2(X),pareto_points(:,2)]);
+
 
 for i=1:10
-    m=m+1;
-    pareto_points
-    
-    
-    
-    F = @(X) f1(X) +f2(X) + AC(X,pareto_points);
-    pareto_points(i,, = ga(F,n,A,b,Aeq,beq,lb,ub)
+    if i > 1
+        %F = @(X) f1(X) +f2(X) + CD(X,pareto_points(:,1:i));
+        F = @(X) f1(X) +f2(X) + CD(X);
+    else
+        F = @(X) f1(X) +f2(X);
+    end
+    vals = ga(F,n,A,b,Aeq,beq,lb,ub);
+    f1_val = real(f1(vals));
+    f2_val = real(f2(vals));
+    pareto_points(i,:) = [f1_val,f2_val];
+end
+
+plot(pareto_points(:,1),pareto_points(:,2),'*')

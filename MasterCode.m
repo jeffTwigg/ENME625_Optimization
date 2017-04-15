12ea1dc856b5262939d02c6ae193f94230cceec0
stephober14 <stephober14@gmail.com>
2017-04-15 15:53:47 -0400

    Add files via upload
    I vectorized the ga using "UseVectorize" option. This meant I had to allow ZDT1 to take an array of points. We will need to make similar changes for the other test problems.

--- MasterCode.m
+++ MasterCode.m
@@ -2,13 +2,13 @@ clear all;
 % close all;
 clc;
 warning off
-%global func V 
+
 
 prompt = 'Which Test Problem Do You Want To Run? \n 1 - ZDT1\n 2 - ZDT2 \n 3 - ZDT3 \n';
 prob = input(prompt);
-prompt2 = 'How Many Chromosomes? Suggest 10-20 ';
+prompt2 = 'How Many Chromosomes? Suggest 30*# variables ';
 nChrome = input(prompt2);
-prompt3 = 'How Many Runs? Suggest >30: ';
+prompt3 = 'How Many Runs? Suggest >10: ';
 nRun = input(prompt3);
 %%
 %prob=1; nChrome = 1; nRun = 40;
@@ -32,31 +32,31 @@ Aeq = []; beq = [];
 LB = zeros(1,nvar); UB = ones(1,nvar);
 
 V = nvar; %Number of design variables
-C = nChrome*nvar; %Number of Chromosomes
 
 %% Initilize Population
 %Initialize the population based on the given lower and upper bounds. Use
 %MATLABs random number generator.
-pop = zeros(C,nvar);
-for k = 1:C
+pop = zeros(nChrome,nvar);
+for k = 1:nChrome
     for j = 1:nvar
         pop(k,j) = LB(j)+(UB(j)-LB(j))*rand;    
     end
 end
-options = optimoptions(@gamultiobj,'InitialPopulationMatrix',pop);
-[Xmoga,Fmoga] = gamultiobj(problem_function,nvar,A,b,Aeq,beq,LB,UB,[],options);
-figure(1)
-plot(Fmoga(:,1),Fmoga(:,2),'bo','LineWidth',2);
-hold on
+% options = optimoptions(@gamultiobj,'InitialPopulationMatrix',pop,'PopulationSize',5000);
+% options = optimoptions(@gamultiobj,'InitialPopulationMatrix',pop);
+% [Xmoga,Fmoga] = gamultiobj(problem_function,nvar,A,b,Aeq,beq,LB,UB,[]);
+% figure(1)
+% % plot(Fmoga(:,1),Fmoga(:,2),'bo','LineWidth',2);
+% hold on
 
 
 Pareto = [];
-options = optimoptions(@ga,'PopulationSize',C);
+options = optimoptions(@ga,'PopulationSize',nChrome,'UseVectorized',true);
 optF =[];
 for gen = 1:nRun
-    Obj_fcn = @(X) fitFCN4(X,problem_function,V,optF);
+    Obj_fcn = @(X) fitFCN5(X,problem_function);
     %Obj_fcn = @(X) fitFCN2(X,problem_function,V);
-    [X] = ga(Obj_fcn,nvar,A,b,Aeq,beq,LB,UB,[],options);
+    [X,fval,exitflag,output] = ga(Obj_fcn,nvar,A,b,Aeq,beq,LB,UB,[],options)
     [optF(gen,:)] = problem_function(X);
     %func = optF(gen,:);
     optX(gen,:) = X;
@@ -64,8 +64,11 @@ end
 [P,~] = prtp(optF);
 Pareto = [Pareto;P];
 
+figure
+hold on;
 plot(Pareto(:,1),Pareto(:,2),'gv','LineWidth',2,'MarkerSize',10)
 plot(optF(:,1),optF(:,2),'r*','LineWidth',2)
 hold on; grid on;
 xlabel('f_1'); ylabel('f_2')
+
 
--- ZDT1.m
+++ ZDT1.m
@@ -1,12 +1,12 @@
 function [ f ] = ZDT1( X )
 %ZDT1 - First test problem from Deb
-%global nfunc
-nvar = length(X);
-F1 = X(1);
+% global nvar nfunc
+nvar = length(X(1,:));
+F1 = X(:,1);
 
-g = 1+(9/(nvar-1))*sum(X(2:end));
-h = 1- sqrt(F1/g);
-F2 = g*h;
+g = 1+(9./(nvar-1)).*sum(X(:,2:end),2);
+h = 1- sqrt(F1./g);
+F2 = g.*h;
 f = [F1,F2];
 nfunc = 2;
 

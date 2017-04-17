8dfb371b8528cbff76567764c0c754b17f8e2667
stephober14 <stephober14@gmail.com>
2017-04-17 09:26:29 -0400

    Add files via upload

--- ZDT1.m
+++ ZDT1.m
@@ -1,15 +1,13 @@
 function [ f ] = ZDT1( X )
 %ZDT1 - First test problem from Deb
-%global nfunc
-nvar = length(X);
-F1 = X(1);
 
-g = 1+(9/(nvar-1))*sum(X(2:end));
-h = 1- sqrt(F1/g);
-F2 = g*h;
+nvar = length(X(1,:));
+F1 = X(:,1);
+
+g = 1+(9./(nvar-1)).*sum(X(:,2:end),2);
+h = 1- sqrt(F1./g);
+F2 = g.*h;
 f = [F1,F2];
-nfunc = 2;
-
 
 end
 
--- ZDT2.m
+++ ZDT2.m
@@ -1,14 +1,13 @@
 function [ f ] = ZDT2( X )
 %ZDT1 - First test problem from Deb
-%global nfunc
-nvar = length(X);
-F1 = X(1);
 
-g = 1+(9/(nvar-1))*sum(X(2:end));
-h = 1- (F1/g)^2;
-F2 = g*h;
+nvar = length(X(1,:));
+F1 = X(:,1);
+
+g = 1+(9./(nvar-1)).*sum(X(:,2:end),2);
+h = 1- (F1./g).^2;
+F2 = g.*h;
 f = [F1,F2];
-nfunc = 2;
 
 
 end

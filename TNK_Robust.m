e852d490fa3ba0d2749f8ba3bc2c38d628a58ff3
BKnisely <bmk9182@gmail.com>
2017-05-03 19:57:30 -0400

    Add files via upload
    Adjusted ENME_parta to include both robust problems. The unconstrained problems are running incredibly slow for some reason and I'm trouble-shooting to figure out way. Could be because of mismatches in files. Other problems work ok.

--- TNK_Robust.m
+++ TNK_Robust.m
@@ -1,18 +1,22 @@
 %For incorporation into exisiting code
-function [f,constraint] = TNK_Robust(X,DP)
+function [f,constraint] = TNK_Robust(X)
 
+global Mmoga DP
 
 f1 = X(:,1);
 f2 = X(:,2);
-g1 = 1  +0.1*cos(16*atan2(X(:,1),X(:,2)))+0.2*sin(DP(:,1)).*cos(DP(:,2)) - X(:,1).^2 -X(:,2).^2;
+g1 = 1  +0.1*cos(16*atan2(X(:,1),X(:,2)))+0.2*sin(1+DP(:,1)).*cos(1+DP(:,2)) - X(:,1).^2 -X(:,2).^2;
 g2 = (X(:,1)-0.5).^2 + (X(:,2)-0.5).^2 -0.5;
 constraint = [g1 g2];
 
 nfunc = 2*ones(length(X(:,1)),1);
 nconstr = 2*ones(length(X(:,1)),1);
 nconstr_lin = 0*ones(length(X(:,1)),1);
-UNCT = 1*ones(length(X(:,1)),1);
+UNCT = 0*ones(length(X(:,1)),1);
 
-f = [f1,f2,constraint,nfunc,nconstr,nconstr_lin,UNCT];
-
+if Mmoga == 0,
+    f = [f1,f2,constraint,nfunc,nconstr,nconstr_lin,UNCT];
+else
+    f = [f1,f2];
+end
 end
